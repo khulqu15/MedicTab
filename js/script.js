9a279a296482e9b8789146ce3bca81cec3623d02
Mohammad Khusnul Khuluq <khusnul.ninno15@gmail.com>
2020-09-04 07:41:25 +0700

    Home page

---
+++ js/script.js
@@ -0,0 +1,33 @@
+$('.articles').slick({
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    dots: false,
+    arrows: false,
+    centerMode: true,
+    autoPlay: true,
+    autoPlaySpeed: 6000,
+    responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1,
+            infinite: true,
+            dots: true
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+    }]
+});
